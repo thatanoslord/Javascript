8eab03d02ef7b1bd0330b90e80bebb9f47a52a62
thatanoslord <sabbarmoussa@gmail.com>
2024-12-16 12:56:02 +0100

    adding a new folders

---
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+// if we want to start a commornt in JS we use two slashes "//"
+// if we want to create a variable in JS we start with "let" then we give it a name 
+let myVariable;
