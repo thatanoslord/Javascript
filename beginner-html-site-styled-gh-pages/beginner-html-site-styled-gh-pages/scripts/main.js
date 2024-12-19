39840adca2a43cf9c7b1bda97a00bccf99a88e6b
thatanoslord <sabbarmoussa@gmail.com>
2024-12-19 15:33:06 +0100

    new

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,6 +1,7 @@
-const myHeading = document.querySelector("h1");
+let myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
-
+// alerts are use to give an attention to a browser user
+alert("this is an alert")
 // if we want to start a commornt in JS we use two slashes "//"
 // if we want to create a variable in JS we start with "let" then we give it a name 
 let myVariable;
