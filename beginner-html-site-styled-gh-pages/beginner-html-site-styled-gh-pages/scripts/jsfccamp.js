47e86cadd7ecdd215be33c1313d51ee7947a8f74
thatanoslord <sabbarmoussa@gmail.com>
2025-01-23 15:08:32 +0100

    newthing

--- beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/jsfccamp.js
+++ beginner-html-site-styled-gh-pages/beginner-html-site-styled-gh-pages/scripts/jsfccamp.js
@@ -3,9 +3,11 @@ const count = 8;
 const rows = [];
 
 function padRow(name) {
-    return name;
+    const test = "Testing";
+    return character + name;
 }
-const call = padRow();
+
+const call = padRow("CamperChan");
 console.log(call);
 
 
