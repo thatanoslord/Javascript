d424a4e3ed44b5cedfec4701b4dfe0339df18def
Your thatanoslord <sabbarmoussa@gmail.com>
2025-07-14 00:15:38 +0100

    documentation

--- new apt/main.js
+++ new apt/main.js
@@ -1,3 +1,11 @@
+// window is for manipulating the browser this object has all thing such as document or consol
+window.alert('Hello world');
+window.print();
+// when I write document I will create and html document 
 document.write('hello world');
+
+// cosole is and object to controle consoles command such as giving error of trying to know a function does it work or not
 console.log("Hello world");
-window.alert('Hello world')
+console.error('test'),
+
+// N.B you can either use window or not it is your choice to 
