d3e83c7ddc87dbc7f0672ce01f329eef82f0743a
Your thatanoslord <sabbarmoussa@gmail.com>
2025-07-14 00:25:16 +0100

    documentation

--- new apt/main.js
+++ new apt/main.js
@@ -6,6 +6,8 @@ document.write('hello world');
 
 // cosole is and object to controle consoles command such as giving error of trying to know a function does it work or not
 console.log("Hello world");
-console.error('test'),
+console.error('test');
 
 // N.B you can either use window or not it is your choice to 
+// JavaScript has a process called automatic semicolon insertion which means after every function the compiler will add a semicolon to your code but does it mean I don't need to write semi colon after myfunction??
+// No and biiiig no because if I compressed my code it all of it will be in one line and thats gonna make a mess
