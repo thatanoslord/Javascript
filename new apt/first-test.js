3310ae4f65c3a767f9074920838815dc55d32bd8
Your thatanoslord <sabbarmoussa@gmail.com>
2025-07-20 00:01:30 +0100

    Fix syntax error in checkString function and update comments for clarity

--- new apt/first-test.js
+++ new apt/first-test.js
@@ -1,5 +1,5 @@
 //quizz
-// 1 b , 2 b, 3 b , 4 c, 5 c
+// 1 b , 2 b, 3 c , 4 c, 5 c
 
 // exercise :1
 let x = "100"
@@ -10,5 +10,13 @@ console.log(x);
 console.log(isSafeInteger(x));
 
 // exercise :2
+function checkString {
+
 let str1 = "Hello world";
-console.log(str1[0], str1[str1.length - 1]); // this will return 2 because the first occurrence of l is at index 2
+console.log(str1[0], str1[str1.length - 1]); 
+console.log(str1.includes("a"));
+console.log(str1.startsWith("Hello"));
+console.log(str1.endsWith("world"));
+}
+
+
