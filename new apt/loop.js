d7d63e24e745cf23a3155e11eaeb6f6aabea1881
Your thatanoslord <sabbarmoussa@gmail.com>
2025-07-30 16:33:05 +0100

    Ajout d'un commentaire explicatif sur l'utilisation des boucles dans loop.js

---
+++ new apt/loop.js
@@ -0,0 +1,5 @@
+/* a loop is a function that is called repeatedly and of the foundation of coding
+instead of repeating a code for 100 using hard code you do a loop so it will help you
+    Right now we will use "for" loop to create a loop
+    how does it work for (star , condition , steps)
+*/ 
