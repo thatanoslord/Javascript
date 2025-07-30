206382dec96746104c29378810f3c67a31a4636c
Your thatanoslord <sabbarmoussa@gmail.com>
2025-07-30 16:40:23 +0100

    Correction de la déclaration de la boucle "for" dans loop.js pour utiliser "let" et amélioration de la lisibilité des commentaires

--- new apt/loop.js
+++ new apt/loop.js
@@ -4,7 +4,8 @@ instead of repeating a code for 100 using hard code you do a loop so it will hel
     how does it work for (star , condition , steps)
 */ 
 // example I want to repeat this sentence 5 times i can either do it hardly and also loop it
-for (i =0 , i < 5, i++) {
+for (let i = 0; i < 5; i++) {
     console.log("Hello world");
-} // this will print "Hello world" 5 times
+} 
+// this will print "Hello world" 5 times
 // the first part is the start of the loop, the second part is the condition that must be true for the loop to continue, and the third part is the step that will be executed after each iteration
