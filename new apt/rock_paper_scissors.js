32c4c067e387ffd0c7ce23f78ef8923070e520fd
Your thatanoslord <sabbarmoussa@gmail.com>
2025-08-21 14:25:41 +0100

    Mise à jour de loop.html pour inclure rock_paper_scissors.js et amélioration de la logique du jeu Pierre-Papier-Ciseaux.

--- new apt/rock_paper_scissors.js
+++ new apt/rock_paper_scissors.js
@@ -1,18 +1,49 @@
 // Write code below 💖
 
-console.log("Welcome to Rock, Paper, Scissors")
-console.log("0 refers to Rock, 1 refers to Paper and 2 refers to Scissors")
+console.log("Welcome to Rock, Paper, Scissors");
+console.log("0 refers to Rock, 1 refers to Paper and 2 refers to Scissors");
 
-let player = 2;
-let computer = Math.floor(Math.random()*3);
+let player = prompt("Choose your sign: ");
+let computer = Math.floor(Math.random() * 3);
 
-if (computer == 0 && player == 0 ){console.log("Draw")} // Rock vs rock
-else if(computer == 1 && player == 1 ){console.log("Draw")} // paper vs paper
-else if(computer == 2 && player == 2 ){console.log("Draw")} // scissors vs scissors
-else if (computer == 1 && player == 0){console.log("lose")} // paper vs rock
-else if (computer == 2 && player == 0){console.log("win")} // scissors vs rock 
-else if (computer == 0 && player == 1){  console.log("win")} // rock vs paper 
-else {console.log("lose")} //rock vs scissors 
+if (computer == 0 && player == 0) {
+  console.log(`Computer choosed: Rock
+    Player choosed: Rock
+    Draw.`);
+} // Rock vs rock
+else if (computer == 1 && player == 1) {
+  console.log(`Computer choosed: Paper
+    Player choosed: Paper
+    Draw.`);
+} // paper vs paper
+else if (computer == 2 && player == 2) {
+  console.log(`Computer choosed: Scissors
+    Player choosed: Scissors
+    Draw.`);
+} // scissors vs scissors
+else if (computer == 1 && player == 0) {
+  console.log(`Computer choosed: Paper
+    Player choosed: Rock
+    Lose.`);
+} // paper vs rock
+else if (computer == 2 && player == 0) {
+  console.log(`Computer choosed: Scissors
+    Player choosed: Rock
+    win.`);
+} // scissors vs rock
+else if (computer == 0 && player == 1) {
+  console.log(`Computer choosed: Rock
+    Player choosed: Paper
+    Win.`);
+} // rock vs paper
+else if (computer == 0 && player == 2) {
+  console.log(`Computer choosed: Rock
+    Player choosed: Scissors
+    lose.`);
+} //rock vs scissors
+else {
+  console.log("Please choose between 0 and 2");
+}
 
 console.log(`This is what computer choose: ${computer}
-and This is what player choose: ${player}`)
+and This is what player choose: ${player}`);
