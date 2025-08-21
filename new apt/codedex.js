cc41d0178428198ec66c7d4414ef4f88d742449b
Your thatanoslord <sabbarmoussa@gmail.com>
2025-08-21 16:30:03 +0100

    Ajout de la logique pour déterminer le jour de la semaine avec des messages appropriés.

--- new apt/codedex.js
+++ new apt/codedex.js
@@ -13,3 +13,19 @@ else if (rating === 3){
 else{
   console.log("Invalid.")
 }
+
+
+
+// which day 
+// Write code below 💖
+let day = Math.floor(Math.random() * 7) + 1;
+if (day === 1 || day === 2 || day === 3 || day === 4){
+  console.log("Not Friday, yet!")
+}
+else if (day === 5){
+  console.log("TGIF 🕺")
+}
+else if (day === 6 || day === 7){
+  console.log("Yay, weekends! 🙌")
+}
+else{console.log("Wait, what day is it?")}
