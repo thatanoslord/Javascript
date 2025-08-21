294afea3f2894f47fc26ab9d1e7aa9726e0bb547
Your thatanoslord <sabbarmoussa@gmail.com>
2025-08-21 15:04:58 +0100

    Ajout d'un nouveau fichier codedex.js avec une logique de notation aléatoire pour évaluer les options de transport.

---
+++ new apt/codedex.js
@@ -0,0 +1,15 @@
+// Write code below 💖
+
+let rating = Math.floor(Math.random()*3+1);
+if (rating === 1){
+  console.log("🌟 is worth walking to.")
+}
+else if (rating === 2){
+  console.log("🌟🌟 is worth driving to.")
+}
+else if (rating === 3){
+  console.log("🌟🌟🌟 is worth flying to.")
+}
+else{
+  console.log("Invalid.")
+}
