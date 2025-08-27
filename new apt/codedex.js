baf53e73c2ea31f59d91b1a793f8f6755dd42193
Your thatanoslord <sabbarmoussa@gmail.com>
2025-08-27 16:07:03 +0100

    Ajout de la logique pour le jeu "Goose or Duck" et implémentation d'un système de devinette pour un nombre chanceux dans codedex.js

--- new apt/codedex.js
+++ new apt/codedex.js
@@ -67,7 +67,7 @@ Now we are going to focus on while loop
 
 */
 
-
+//goose or duck 
 
 while (randomNumber !== 7) {
   console.log("Duck 🦆");
@@ -75,3 +75,15 @@ while (randomNumber !== 7) {
 }
 
 console.log("Goose! 🦢");
+
+// lucky number
+
+
+const luckyNumber = 7
+
+let guess = Math.floor(Math.random() * 10) + 1;
+while (guess !== luckyNumber){
+  console.log(`Nope, it isn't ${guess}`)
+  guess = Math.floor(Math.random() * 10) + 1
+}
+console.log(`My lucky number is ${guess}`)
