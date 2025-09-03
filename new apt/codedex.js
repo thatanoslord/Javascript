fe3cabecf88a0de21dc95ae33760b48193d4bb70
Your thatanoslord <sabbarmoussa@gmail.com>
2025-09-03 17:43:24 +0100

    Ajout de la logique pour un tirage au sort avec 10 essais et affichage des résultats dans le jeu Swag Raffle.

--- new apt/codedex.js
+++ new apt/codedex.js
@@ -124,3 +124,21 @@ for (let i = 10; i > 0; i--) {
   console.log(i);
 }
 console.log("To infinity and beyond! 🚀");
+
+//Swag Raffle
+
+// Write code below 💖
+
+let count = 0;
+let targetNumber = Math.floor(Math.random()*100);
+while(count < 10){
+  let drawNumber = Math.floor(Math.random()*100);
+  if ( drawNumber === targetNumber){
+    count++
+    console.log("Match")
+  }
+  else{
+    console.log("No match")
+  }
+}
+console.log(`The number ${targetNumber} was found 10 times.`)
