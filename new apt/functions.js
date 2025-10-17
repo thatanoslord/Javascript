20981ab16b23f0d9be017e909f6de1342dbd5ebb
Your thatanoslord <sabbarmoussa@gmail.com>
2025-10-17 23:49:13 +0100

    adding functions to this files

--- new apt/functions.js
+++ new apt/functions.js
@@ -33,3 +33,26 @@ function personhours(age) {
   return hours;
 }
 console.log(`your Age in hours is: ${personhours()}`);
+
+// Function of a loan Qualification checker
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan."
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan."
+  } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+    return "You qualify for a car loan."
+  } else {
+    return "You don't qualify for any loans."
+  }
+}
