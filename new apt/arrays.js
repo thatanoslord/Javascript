c59b3dacd638c1c7cf9d4112a4205b4d5f1b4c5f
Your thatanoslord <sabbarmoussa@gmail.com>
2025-10-23 14:30:44 +0100

    Ajout de fonctions pour gérer le menu des déjeuners : ajout, suppression et affichage des éléments.

--- new apt/arrays.js
+++ new apt/arrays.js
@@ -40,3 +40,33 @@ console.log("On second thought, maybe we should be more health conscious.");
 shoppingList.shift();
 shoppingList[0] = "Canola Oil";
 console.log(getShoppingListMsg(shoppingList));
+
+
+// lunch picker program 
+const lunches= [];
+function addLunchToEnd(arr, item){
+  arr.push(item);
+  console.log(`${item} added to the end of the lunch menu.`)
+  return arr
+}
+console.log(addLunchToEnd(lunches, "Tacos"))
+
+function addLunchToStart(arr, item){
+  arr.unshift(item);
+  console.log(`${item} added to the start of the lunch menu.`)
+  return arr
+}
+console.log(addLunchToEnd(lunches, "Sushi"))  
+
+function removeLastLunch(arr){
+  let removedItem = arr.pop();
+  
+  if (arr.length == []){
+ return console.log("No lunches to remove.")
+}
+else{console.log(`${removedItem} removed from the end of the lunch menu.`)}
+return arr
+}
+console.log(removeLastLunch(lunches))
+ 
+ 
