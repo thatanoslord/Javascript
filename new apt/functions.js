f31d34f0a2df58e6c6a6ecec1979a546792ee855
Your thatanoslord <sabbarmoussa@gmail.com>
2025-11-26 14:57:11 +0100

    Ajout d'une fonction pour mettre à jour une collection de disques avec gestion des propriétés et des pistes.

--- new apt/functions.js
+++ new apt/functions.js
@@ -305,4 +305,43 @@ function getResults(quest, compAnswer){
 
 console.log(getResults(randomQuest, getRandomComputerChoice(choice)))  
 
-   
+// Build a Record Collection
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+
+function updateRecords (records, id, prop, value){
+  if (value === ""){
+    delete  records[id][prop]
+    return records
+  }
+  else if (prop != "tracks" && value != ""){
+    records[id][prop] = value;
+    return records
+  }
+  else if(prop === "tracks" && value != "" && !records[id].tracks){
+    records[id].tracks = [value]
+    return records
+  }
+  else if (prop ==="tracks" && value !== ""){
+    records[id].tracks.push(value)
+    return records
+  }
+} 
